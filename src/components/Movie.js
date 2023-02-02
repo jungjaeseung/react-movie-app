9077da295f0931932c07c991b4741a23e7543e60
3Harang <jungjaeseung9475@gmail.com>
2023-02-02 23:44:49 +0900

    styling
    movie.js home.js styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,48 @@
 import propTypes from "prop-types";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, genres, summary }) {
+function Movie({ id, coverImg, title, genres, summary, year, rating }) {
+  const [isOn, setIsOn] = useState(false);
   return (
     <div className={styles.movie}>
-      <img src={coverImg} alt={title} />
-      <h3 className={styles.title}>
-        <Link
-          to={`/movie/${id}`}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          {title}
-        </Link>
-      </h3>
-      <ul>
-        {genres.map((genre, index) => (
-          <li key={index}>{genre}</li>
-        ))}
-      </ul>
-      <p>Summary : {summary === "" ? "no summary" : summary}</p>
+      <Link
+        onMouseOver={() => {
+          setIsOn(true);
+        }}
+        onMouseOut={() => {
+          setIsOn(false);
+        }}
+        className={styles.imgLink}
+        to={`/movie/${id}`}
+      >
+        {isOn ? (
+          <div className={styles.blackbox}>
+            <span>★{rating}</span>
+          </div>
+        ) : null}
+        <img src={coverImg} alt={title} />
+      </Link>
+
+      <div>
+        <h2 className={styles.title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <span className={styles.year}>{year}</span>
+        <p>
+          {summary === ""
+            ? "no summary"
+            : summary.length > 180
+            ? `${summary.slice(0, 180)}...`
+            : summary}
+        </p>
+        <ul className={styles.genres}>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
@@ -30,6 +53,7 @@ Movie.propTypes = {
   title: propTypes.string.isRequired,
   genres: propTypes.arrayOf(propTypes.string).isRequired,
   summary: propTypes.string.isRequired,
+  year: propTypes.number.isRequired,
 };
 
 export default Movie;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
   const getMovies = async () => {
     const json = await (
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
       )
     ).json();
     setMovies(json.data.movies);
@@ -17,12 +17,17 @@ function Home() {
   useEffect(() => {
     getMovies();
   }, []);
+  // useEffect(() => {
+  //   console.log(movies);
+  // }, [movies]);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <h1>Loading...</h1>
+        </div>
       ) : (
-        <div className={styles.movieContainer}>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
@@ -31,6 +36,8 @@ function Home() {
               title={movie.title}
               genres={movie.genres}
               summary={movie.summary}
+              year={movie.year}
+              rating={movie.rating}
             />
           ))}
         </div>
